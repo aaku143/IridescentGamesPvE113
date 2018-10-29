be75a51cc83d713faea14515962d4f96a4ad54f8
YeeJay Ng <yeejayng10@gmail.com>
2018-10-28 18:30:53 -0700

    Adding blank player information

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private int health;
+	// Use this for initialization
+	void Start () {
+        health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// check for collision with projectile/enemy and decrease health appropriately
+	}
+}
