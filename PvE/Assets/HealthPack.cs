e2a07ce97375590bf1a89ca18f277005650bca84
yeejayng <yeejayng10@gmail.com>
2018-11-14 14:48:52 -0800

    Health Packs

---
+++ PvE/Assets/HealthPack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour {
+    //tag = HealthPack
+    // Use this for initialization
+
+    int healAmount = 5;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            if ( player.maxHealth < player.health + healAmount )
+            {
+                player.health = player.maxHealth;
+            }
+            else
+            {
+                player.health += healAmount;
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
